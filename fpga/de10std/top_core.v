8d5f2474d2aa1ba07239d223785f1aa26139203b
Vasyl Vavrychuk <vvavrychuk@gmail.com>
2018-06-19 12:09:06 +0300

    de10std: added platform design
    It implements:
    
    - on chip ram for storing sniffing results
    - jtag master bridge for accessing hardware from PC
    - Avalon to external interface from jtag master to usb_sniffer module in verilog
    - external to Avalong interface from usb_sniffer to ram

--- fpga/de10std/top_core.v
+++ fpga/de10std/top_core.v
@@ -6,7 +6,7 @@ module top_core
     // ULPI PHY clock (60MHz)
     input           clk_i,
     input           rst_i,
-  
+
     // ULPI Interface
     input  [7:0]    ulpi_data_i,
     output [7:0]    ulpi_data_o,
@@ -63,24 +63,59 @@ u_ulpi
 );
 
 //-----------------------------------------------------------------
+// SoC System
+//-----------------------------------------------------------------
+wire        periph_ei_bus_enable;
+wire [7:0]  periph_ei_address;
+wire [31:0] periph_ei_read_data;
+wire [31:0] periph_ei_write_data;
+wire        periph_ei_rw;
+wire        periph_ei_acknowledge;
+
+wire [29:0] ram_ei_address;
+wire [3:0]  ram_ei_byte_enable;
+wire        ram_ei_write;
+wire [31:0] ram_ei_write_data;
+wire        ram_ei_acknowledge;
+
+soc_system
+u0
+(
+    .clk_clk                                        (clk_i),                 //                              clk.clk
+    .reset_reset_n                                  (~rst_i),                //                            reset.reset_n
+    .periph_bridge_0_external_interface_bus_enable  (periph_ei_bus_enable),  // periph_bridge_0_external_interface.bus_enable
+    .periph_bridge_0_external_interface_address     (periph_ei_address),     //                                   .address
+    .periph_bridge_0_external_interface_read_data   (periph_ei_read_data),   //                                   .read_data
+    .periph_bridge_0_external_interface_write_data  (periph_ei_write_data),  //                                   .write_data
+    .periph_bridge_0_external_interface_rw          (periph_ei_rw),          //                                   .rw
+    .periph_bridge_0_external_interface_acknowledge (periph_ei_acknowledge), //                                   .acknowledge
+    .ram_bridge_0_external_interface_address        (ram_ei_address),        //    ram_bridge_0_external_interface.address
+    .ram_bridge_0_external_interface_byte_enable    (ram_ei_byte_enable),    //                                   .byte_enable
+    .ram_bridge_0_external_interface_write          (ram_ei_write),          //                                   .write
+    .ram_bridge_0_external_interface_write_data     (ram_ei_write_data),     //                                   .write_data
+    .ram_bridge_0_external_interface_acknowledge    (ram_ei_acknowledge)     //                                   .acknowledge
+);
+
+//-----------------------------------------------------------------
 // USB Sniffer
 //-----------------------------------------------------------------
+wire [31:0] sniffer_addr_w = {2'b0, ram_ei_address};
+wire sniffer_stb_w;
+wire sniffer_we_w;
 
 usb_sniffer
 u_sniffer
 (
     .clk_i(clk_i),
     .rst_i(rst_i),
-    
+
     // Peripheral Interface
-    /*
-    .addr_i(ftdi_address_w[7:0]),
-    .data_i(ftdi_data_w),
-    .data_o(periph_data_r),
-    .we_i(ftdi_we_w),
-    .stb_i(periph_stb_w),
-    .ack_o(periph_ack_w),
-    */
+    .addr_i(periph_ei_address),
+    .data_i(periph_ei_write_data),
+    .data_o(periph_ei_read_data),
+    .we_i(!periph_ei_rw),
+    .stb_i(periph_ei_bus_enable),
+    .ack_o(periph_ei_acknowledge),
 
     // UTMI Interface
     .utmi_rxvalid_i(utmi_rxvalid),
@@ -93,18 +128,18 @@ u_sniffer
     .utmi_xcvrselect_o(xcvrselect_w),
     .utmi_termselect_o(termselect_w),
     .utmi_dppulldown_o(dppulldown_w),
-    .utmi_dmpulldown_o(dmpulldown_w)
+    .utmi_dmpulldown_o(dmpulldown_w),
 
     // Wishbone (Slave)
-    /*
     .mem_addr_o(sniffer_addr_w),
-    .mem_sel_o(sniffer_sel_w),
-    .mem_data_o(sniffer_data_w),
+    .mem_sel_o(ram_ei_byte_enable),
+    .mem_data_o(ram_ei_write_data),
     .mem_stb_o(sniffer_stb_w),
     .mem_we_o(sniffer_we_w),
-    .mem_stall_i(sniffer_stall_w),
-    .mem_ack_i(sniffer_ack_w)
-    */
+    .mem_stall_i(1'b0),
+    .mem_ack_i(ram_ei_acknowledge)
 );
+
+assign ram_ei_write = sniffer_stb_w && sniffer_we_w;
 
 endmodule
