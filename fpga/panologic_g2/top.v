06c976d505a79fe490e4323985aef5cc04a8dcbd
Skip Hansen <skip@gfrn.org>
2019-10-31 18:54:08 -0700

    Updated pano.ucf, changed part to xc6slx150, removed all minispartan6plus specific I/O.

--- fpga/panologic_g2/top.v
+++ fpga/panologic_g2/top.v
@@ -6,149 +6,13 @@ module top
     // 50MHz clock
     input           clk,
 
-    // LED
-    output [7:0]    leds,
-
-    // SPI Flash
-    output          flash_cs,
-    output          flash_cclk,
-    output          flash_mosi,
-    input           flash_miso,
-
-    // ADC
-    output          ad_cs,
-    output          ad_sclk,
-    output          ad_din,
-    input           ad_dout,
-
-    // DIP Switches
-    input [3:0]     sw,    
-
-    // SD card
-    output          sd_clk,
-    output          sd_cd_dat3,
-    inout           sd_dat0,
-    inout           sd_dat1,
-    inout           sd_dat2,  
-    output          sd_cmd,
-
-    // SDRAM
-    inout           udqm,
-    inout           sdram_clk,
-    inout           cke,
-    inout           bs1,
-    inout           bs0,
-    inout           sdram_csn,
-    inout           rasn,
-    inout           casn,
-    inout           wen,
-    inout           ldqm,
-    inout [12:0]    a,
-    inout [15:0]    d,
-
-    output          audio1,
-    output          audio2,
- 
-    // I2C
-    inout           fpga_scl,
-    inout           fpga_sda,
-
-    // FTDI
-    inout           ftdi_rxf,
-    inout           ftdi_txe,
-    inout           ftdi_siwua,
-    inout           ftdi_wr,
-    inout           ftdi_rd,
-    inout [7:0]     ftdi_d, // TXD, RXD
-
-    // HDMI In (J2)
-    //input           tmds_in_p0,
-    //input           tmds_in_n0,
-    //input           tmds_in_p1,
-    //input           tmds_in_n1,
-    //input           tmds_in_p2,
-    //input           tmds_in_n2,
-    //input           tmds_in_clk_p,
-    //input           tmds_in_clk_n,
-
-    // HDMI Out (J3)
-    //output          tmds_out_p0,
-    //output          tmds_out_n0,
-    //output          tmds_out_p1,
-    //output          tmds_out_n1,
-    //output          tmds_out_p2,
-    //output          tmds_out_n2,
-    //output          tmds_out_clk_p,
-    //output          tmds_out_clk_n,
-
-    // Port a i/o pins
-    inout           porta0,
-    inout           porta1,
-    inout           porta2,
-    inout           porta3,
-    inout           porta4,
-    inout           porta5,
-    inout           porta6,
-    inout           porta7,
-    inout           porta8,
-    inout           porta9,
-    inout           porta10,
-    inout           porta11,
-
-    // Port b i/o pins
-    inout           portb0,
-    inout           portb1,
-    inout           portb2,
-    inout           portb3,
-    inout           portb4,
-    inout           portb5,
-    inout           portb6,
-    inout           portb7,
-    inout           portb8,
-    inout           portb9,
-    inout           portb10,
-    inout           portb11,
-
     // USB ULPI Interface
     input           usb_clk60,
     output          usb_ulpi_stp,
     input           usb_ulpi_dir,
     input           usb_ulpi_nxt,
-    inout [7:0]     usb_ulpi_data,
+    inout [7:0]     usb_ulpi_data
 
-    // Port d i/o pins
-    inout           portd0,
-    inout           portd1,
-    inout           portd2,
-    inout           portd3,
-
-    // Port e i/o pins
-    inout           porte0,
-    inout           porte1,
-    inout           porte2,
-    inout           porte3,
-    inout           porte4,
-    inout           porte5,
-    inout           porte6,
-    inout           porte7,
-    inout           porte8,
-    inout           porte9,
-    inout           porte10,
-    inout           porte11,
-
-    // Port f i/o pins
-    inout           portf0,
-    inout           portf1,
-    inout           portf2,
-    inout           portf3,
-    inout           portf4,
-    inout           portf5,
-    inout           portf6,
-    inout           portf7,
-    inout           portf8,
-    inout           portf9,
-    inout           portf10,
-    inout           portf11
 );
 
 //-----------------------------------------------------------------
@@ -229,18 +93,8 @@ u_core
     .ulpi_data_o(ulpi_out_w),
     .ulpi_dir_i(usb_ulpi_dir),
     .ulpi_nxt_i(usb_ulpi_nxt),
-    .ulpi_stp_o(ulpi_stp_w),
+    .ulpi_stp_o(ulpi_stp_w)
 
-    // FTDI (async FIFO interface)
-    .ftdi_rxf(ftdi_rxf),
-    .ftdi_txe(ftdi_txe),
-    .ftdi_siwua(ftdi_siwua),
-    .ftdi_wr(ftdi_wr),
-    .ftdi_rd(ftdi_rd),
-    .ftdi_d(ftdi_d),
-
-    // MISC
-    .leds(leds)
 );
 
 //-----------------------------------------------------------------
--- fpga/panologic_g2/top_core.v
+++ fpga/panologic_g2/top_core.v
@@ -12,18 +12,8 @@ module top_core
     output [7:0]    ulpi_data_o,
     input           ulpi_dir_i,
     input           ulpi_nxt_i,
-    output          ulpi_stp_o,
+    output          ulpi_stp_o
 
-    // FTDI
-    inout           ftdi_rxf,
-    inout           ftdi_txe,
-    inout           ftdi_siwua,
-    inout           ftdi_wr,
-    inout           ftdi_rd,
-    inout [7:0]     ftdi_d,
-
-    // MISC
-    output [7:0]    leds
 );
 
 //-----------------------------------------------------------------
@@ -34,11 +24,6 @@ parameter       UART_BAUD           = 115200;
 parameter       SPI_CLK_KHZ         = CLK_KHZ / 5;
 parameter       BOOT_VECTOR         = 32'h00000000;
 parameter       ISR_VECTOR          = 32'h00000010;
-
-//-----------------------------------------------------------------
-// Registers / Wires
-//-----------------------------------------------------------------
-wire [7:0]  ftdi_gpio_w;
 
 //-----------------------------------------------------------------
 // ULPI
@@ -88,68 +73,6 @@ u_ulpi
     .utmi_linestate_o(utmi_linestate)
 );
 
-//-----------------------------------------------------------------
-// FTDI Interface
-//-----------------------------------------------------------------
-wire[31:0]  ftdi_address_w;
-wire[31:0]  ftdi_data_w;
-wire[31:0]  ftdi_data_r;
-wire[3:0]   ftdi_sel_w;
-wire        ftdi_we_w;
-wire        ftdi_stb_w;
-wire        ftdi_cyc_w;
-wire        ftdi_stall_w = 1'b0;
-wire        ftdi_ack_w;
-
-ftdi_if
-u_ftdi
-(
-    .clk_i(clk_i),
-    .rst_i(rst_i),
-
-    // FTDI (async FIFO interface)
-    .ftdi_rxf_i(ftdi_rxf),
-    .ftdi_txe_i(ftdi_txe),
-    .ftdi_siwua_o(ftdi_siwua),
-    .ftdi_wr_o(ftdi_wr),
-    .ftdi_rd_o(ftdi_rd),
-    .ftdi_d_io(ftdi_d),
-
-    .gp_o(ftdi_gpio_w),
-    .gp_i(ftdi_gpio_w),
-
-    // Wishbone
-    .mem_addr_o(ftdi_address_w),
-    .mem_data_o(ftdi_data_w),
-    .mem_data_i(ftdi_data_r),
-    .mem_sel_o(ftdi_sel_w),
-    .mem_we_o(ftdi_we_w),
-    .mem_stb_o(ftdi_stb_w),
-    .mem_cyc_o(ftdi_cyc_w),
-    .mem_ack_i(ftdi_ack_w),
-    .mem_stall_i(ftdi_stall_w)
-);
-
-//-----------------------------------------------------------------
-// Wishbone Interface Select
-//-----------------------------------------------------------------
-wire        mem_stb_w     = ftdi_stb_w & ~ftdi_address_w[31];
-wire [31:0] mem_data_r;
-wire        mem_ack_w;
-
-wire        periph_stb_w  = ftdi_stb_w & ftdi_address_w[31];
-wire [31:0] periph_data_r;
-wire        periph_ack_w;
-
-reg sel_q;
-always @(posedge clk_i or posedge rst_i)
-if (rst_i)
-    sel_q  <= 1'b0;
-else if (ftdi_stb_w)
-    sel_q  <= ftdi_address_w[31];
-
-assign ftdi_data_r  = sel_q ? periph_data_r : mem_data_r;
-assign ftdi_ack_w   = sel_q ? periph_ack_w  : mem_ack_w;
 
 //-----------------------------------------------------------------
 // USB Sniffer
@@ -170,12 +93,12 @@ u_sniffer
     .rst_i(rst_i),
     
     // Peripheral Interface
-    .addr_i(ftdi_address_w[7:0]),
-    .data_i(ftdi_data_w),
-    .data_o(periph_data_r),
-    .we_i(ftdi_we_w),
-    .stb_i(periph_stb_w),
-    .ack_o(periph_ack_w),
+    .addr_i(8'b0),
+    .data_i(32'b0),
+    .data_o(),
+    .we_i(1'b0),
+    .stb_i(1'b0),
+    .ack_o(),
 
     // UTMI Interface
     .utmi_rxvalid_i(utmi_rxvalid),
@@ -219,22 +142,17 @@ u_ram
     .acka_o(sniffer_ack_w),
 
     // Port B - External Port
-    .clkb_i(clk_i),
-    .rstb_i(rst_i),
-    .stbb_i(mem_stb_w),
-    .web_i(ftdi_we_w),
-    .selb_i(ftdi_sel_w),
-    .addrb_i(ftdi_address_w),
-    .datab_i(ftdi_data_w),
-    .datab_o(mem_data_r),
-    .ackb_o(mem_ack_w)    
+    .clkb_i(1'b0),
+    .rstb_i(1'b0),
+    .stbb_i(1'b0),
+    .web_i(1'b0),
+    .selb_i(4'b0),
+    .addrb_i(32'b0),
+    .datab_i(32'b0),
+    .datab_o(),
+    .ackb_o()    
 );
 
 assign sniffer_stall_w = 1'b0;
-
-//-----------------------------------------------------------------
-// LED
-//-----------------------------------------------------------------
-assign leds = ftdi_gpio_w;
 
 endmodule
